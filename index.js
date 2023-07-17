051e6b9ace77c62f55596eccb7d738e712120144
Alex Spieslechner <alexander.spieslechner@gmail.com>
2023-07-17 15:12:19 +0200

    refactor: get it working on windows and unix

--- index.js
+++ index.js
@@ -1,106 +1,83 @@
 import fs from "node:fs/promises";
 import { spawn, spawnSync, exec, execSync } from "node:child_process";
 
-function getDuration(start, end) {
-  return ((end - start) / 1000).toFixed(2);
-}
-
 const FILE_PATH = "src/app/App.tsx";
 
 const BACKOFF_TIME_MS = 5000;
 
-const YARN_SCRIPTS = [
-  ["install"],
-  // ["build"],
-  // ["test", "run"],
-  // ["lint"],
-  // ["format"],
-  // ["tsc", "--noEmit"],
+const SCRIPTS = [
+  ["rm", ["-r", "node_modules"]],
+  ["yarn", ["install"]],
+  ["yarn", ["build"]],
+  ["yarn", ["test", "run"]],
+  ["yarn", ["lint"]],
+  ["yarn", ["format"]],
+  ["yarn", ["tsc", "--noEmit"]],
 ];
 
-async function test_static() {
-  return new Promise((resolve, reject) => {
-    try {
-      // disabled for now, as this breaks the hot-reload test
-      // const s = performance.now();
-      // console.log("clearing node_modules...");
-      // execSync("rm -rf node_modules");
-      // console.log(`${getDuration(s, performance.now())}s`);
+function getDuration(start, end) {
+  return ((end - start) / 1000).toFixed(2);
+}
 
-      for (const args of YARN_SCRIPTS) {
-        const start = performance.now();
-        console.log(`yarn ${args.join(" ")}...`);
-        spawnSync("yarn", args, {
-          stdio: ["ignore", "pipe", "pipe"],
-          shell: true,
-        });
-        console.log(`${getDuration(start, performance.now())}s`);
-      }
-      resolve();
-    } catch (e) {
-      reject(e);
+function debounce(cb, wait = 5000) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => cb(args), wait);
+  };
+}
+
+async function test_static() {
+  return new Promise((resolve) => {
+    for (const script of SCRIPTS) {
+      const [command, args] = script;
+      const start = performance.now();
+      console.log(`${command} ${args.join(" ")}...`);
+      spawnSync(command, args, {
+        stdio: ["ignore", "pipe", "pipe"],
+        shell: true,
+      });
+      console.log(`${getDuration(start, performance.now())}s`);
     }
+    resolve();
   });
 }
 
 async function test_dev() {
   console.log("yarn dev...");
   const searchRegExp = new RegExp(/imports crawl ended/gi);
-  const controller = new AbortController();
+  const start = performance.now();
   const child = spawn("yarn", ["dev", "--debug"], {
     shell: true,
-    stdio: ["pipe", "pipe", "pipe"],
-    signal: controller.signal,
+    stdio: ["ignore", "pipe", "pipe"],
   });
-  if (!child) {
-    throw new Error("Failed to spawn yarn dev process");
-  }
-  const start = performance.now();
 
-  let timeout;
-
-  function handleMessage(data) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      console.log("didnt receive input for 5s assuming dev server is ready");
-      child.stderr.removeListener("data", handleMessage);
-      console.log(
-        `${getDuration(start, performance.now() - BACKOFF_TIME_MS)}s`
-      );
-      test_hotreload(child, controller);
-    }, BACKOFF_TIME_MS);
-  }
+  const handleMessage = debounce(() => {
+    console.log(`${getDuration(start, performance.now() - BACKOFF_TIME_MS)}s`);
+    child.stderr.removeListener("data", handleMessage);
+    test_hotreload(child);
+  });
 
   child.stderr.addListener("data", handleMessage);
 }
 
-async function test_hotreload(childProcess, controller) {
+async function test_hotreload(child) {
   console.log("hot-reload...");
 
-  let timeout;
-  function handleMessage(data) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      console.log(
-        `${getDuration(start, performance.now() - BACKOFF_TIME_MS)}s`
-      );
-      exec(`git checkout ${FILE_PATH}`);
+  const handleMessage = debounce(() => {
+    console.log(`${getDuration(start, performance.now() - BACKOFF_TIME_MS)}s`);
+    const gitProcess = spawn("git", ["checkout", FILE_PATH], {
+      shell: true,
+    });
+    gitProcess.on("close", () => {
       process.exit(0);
-    }, BACKOFF_TIME_MS);
-  }
-  childProcess.stderr.addListener("data", handleMessage);
+    });
+  });
+  child.stderr.addListener("data", handleMessage);
   const start = performance.now();
   const content = await fs.readFile(FILE_PATH, "utf-8");
   const updatedContent = content.replace("Sitelife", Math.random());
   fs.writeFile(FILE_PATH, updatedContent);
-}
-
-function backoff(cb, timeout = BACKOFF_TIME_MS) {
-  let timeout;
-  return () => {
-    clearTimeout(timeout);
-    timeout = setTimeout(cb, timeout);
-  };
 }
 
 (async () => {
